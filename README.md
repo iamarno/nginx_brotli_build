4036a57c5c0fbe66933fc0df6d400b3e30a35270
Arno Herrmann <arno.herrmann@newsfactory.de>
2022-09-22 20:17:41 +0200

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# nginx_brotli_build
+build the nginx brotli module binary on amazon linux  
+
+![build](https://github.com/iamarno/nginx_brotli_build/actions/workflows/build.yml/badge.svg)
---
+++ build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+###versions
+
+nginx_version="1.22.0"
+module_version="1.0.0rc"
+
+# build nginx module
+echo "build with nginx version $nginx_version"
+
+# download nginx binary from nginx.org
+wget http://nginx.org/download/nginx-$nginx_version.tar.gz
+
+# extract
+tar xfv nginx-$nginx_version.tar.gz
+
+# download module source
+wget https://github.com/google/ngx_brotli/archive/refs/tags/v$module_version.tar.gz
+
+# extract
+tar xfv ngx_brotli-$module_version.tar.gz
+
+# cd
+cd nginx-$nginx_version
+
+# ./configure
+./configure --with-compat --add-dynamic-module=../ngx_brotli-$module_version/ --with-cc-opt=-Wno-stringop-overread
+
+# make
+make
+
+# make install
+make install
+
+# copy module to module directory
+cp objs/ngx_http_brotli_filter_module.so /module/.
+cp objs/ngx_http_brotli_static_module.so /module/.
+
+exit 0
